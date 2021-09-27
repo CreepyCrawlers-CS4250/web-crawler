5972b27e5b7c7b4cf2d6c5f3c5c584d034698537
matthewkhle <mattmattmattle@gmail.com>
2021-09-26 19:04:28 -0700

    Update web-crawler.py
    - adds files to repository folder
    - adds link and number of outlinks to report.csv

--- web-crawler.py
+++ web-crawler.py
@@ -3,33 +3,35 @@ from bs4 import BeautifulSoup
 import csv
 
 all_links = []
+counter = 0
 
 def add_to_report(link, num_outlinks):
     try:
-        with open('report.csv', 'a') as csvfile:
+        with open('report.csv', 'a', newline='') as csvfile:
             csvwriter = csv.writer(csvfile)
             csvwriter.writerow([link, num_outlinks])
     except:
         pass   
 
-# def add_to_repository(title, header, link):
-#     # do we make it .txt or .html???
-#     try:
-#         file_name = title + ';' + header + ';' + link + '.html'
-#         f = open(file_name, "x")
-#     except:
-#         print("error. file already exists")
-#         pass
+def add_to_repository(text):
+    # do we make it .txt or .html???
+    global counter
+    file_name = 'repository/{}.html'.format(counter)
+    f = open(file_name, "w", encoding="utf-8")
+    f.write(text)
+    counter += 1
 
 def get_outlinks(links):
     outlinks = []
     for link in links:
             if 'href' in link.attrs:
-                if link['href'] not in outlinks:
-                    outlinks.append(link['href'])
+                if 'https://' in link['href'] or 'http://' in link['href']:
+                    if link['href'] not in outlinks:
+                        outlinks.append(link['href'])
     return outlinks
 
 def creepy_crawler(link):
+    # print(link)
     # GET request on seed
     req = requests.get(link)
 
@@ -38,15 +40,23 @@ def creepy_crawler(link):
     soup = BeautifulSoup(req.text, "html.parser")
 
     # Create a new file and save in repository folder
-    # add_to_repository(soup.title.text, soup.h1.text, link)
+    add_to_repository(req.text)
 
     # Get all links for seed
     outlinks = get_outlinks(soup('a'))
     all_links.extend([outlink for outlink in outlinks if outlink not in all_links])
+    #print(all_links)
     # Add seed to report.csv
     add_to_report(link, len(outlinks))
 
+    global counter
+    if counter > 500:
+        return "done"
+    else:
+        creepy_crawler(all_links.pop(0))
+
 seed = "https://en.wikipedia.org"
+# all_links.append(seed)
 creepy_crawler(seed)
 
 # print(soup.prettify())
